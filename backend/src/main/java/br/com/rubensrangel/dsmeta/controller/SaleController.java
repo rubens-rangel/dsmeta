80c81355c4d27e2f130a66d6e46c396c939633e0
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-07-15 15:35:50 -0300

    Date select

--- backend/src/main/java/br/com/rubensrangel/dsmeta/controller/SaleController.java
+++ backend/src/main/java/br/com/rubensrangel/dsmeta/controller/SaleController.java
@@ -3,9 +3,13 @@ package br.com.rubensrangel.dsmeta.controller;
 import br.com.rubensrangel.dsmeta.entities.Sale;
 import br.com.rubensrangel.dsmeta.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -17,7 +21,9 @@ public class SaleController {
     private SaleService service;
 
     @GetMapping
-    public List<Sale> findSales(){
-        return service.findSales();
+    public Page<Sale> findSales(@RequestParam(value = "minDate",defaultValue = "") String minDate,
+                                @RequestParam(value = "maxDate",defaultValue = "")String maxDate,
+                                Pageable pageable){
+        return service.findSales(minDate,maxDate,pageable);
     }
 }

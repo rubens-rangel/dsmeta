e7edb0330c8a738088e15889fa59f9e913dfccda
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-07-16 15:23:03 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
 import SalesCard from "./components/SalesCard"
-import NotificationButton from "./components/NotificationButton"
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
 
 function App() {
     return(
     <>
+    <ToastContainer/>
   <Header />
   <main>
       <section id="sales">
